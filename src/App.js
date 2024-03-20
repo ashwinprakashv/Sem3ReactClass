354eee875d8820ed831946bfa3868b8bb8872caa
Ashwin Prakash V <47878521+ashwinprakashv@users.noreply.github.com>
2024-03-20 01:59:06 -0400

    Login Page with alert

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
-// src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './components/Login';
-import Index from './components/Index';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col, Form, Button, Alert } from 'react-bootstrap';
 
-const App = () => {
+function App() {
+  const [showAlert, setShowAlert] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault(); 
+    setShowAlert(true);
+    setTimeout(() => {
+      setShowAlert(false);
+    }, 3000);
+  };
+
   return (
-    <Router>
-<Routes>
-  <Route path="/" element={<Login />} />
-  <Route path="/index.html" element={<Index />} />
-</Routes>
+    <Container style={{ height: '100vh' }} className="d-flex align-items-center justify-content-center">
+      <Row>
+        <Col md={12}>
+          {showAlert && <Alert variant="success">Login Successfull</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label className="fw-bold">Username</Form.Label>
+              <Form.Control type="text" placeholder="Enter username" required className="mb-3"/>
+            </Form.Group>
 
-    </Router>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label className="fw-bold">Password</Form.Label>
+              <Form.Control type="password" placeholder="Enter Password" required />
+            </Form.Group>
+            <Button variant="primary" type="submit" className="mt-3">
+              Log In
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
-};
+}
 
 export default App;
